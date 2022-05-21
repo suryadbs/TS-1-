55f9fa2042bf9a159b19ba8f78f9161d8a694f0d
suryadbs <105383429+suryadbs@users.noreply.github.com>
2022-05-21 21:53:32 +0530

    Add files via upload

---
+++ TS-2 PROGRAM 4.py
@@ -0,0 +1,7 @@
+print('*'*40)
+first="Jimmy"
+second="Johny"
+print(' Words Before Swapping :',first,'\t',second,'\n')
+first,second=second,first
+print(' Words After Swapping  :',first,'\t',second,'\n')
+print('*'*40)
