8180f89b2a51c484bba8fd97dc9cf0c7856cedb2
suryadbs <105383429+suryadbs@users.noreply.github.com>
2022-05-22 13:03:49 +0530

    Add files via upload

---
+++ TS-2 PROGRAM 12.py
@@ -0,0 +1,11 @@
+print('^'*25)
+def same(a,b):
+    if len(a)==len(b):
+        return True
+    else:
+        return False
+x=input('Enter String 1 :')
+y=input('Enter String 2 :')
+print()
+print(same(x,y))
+print('^'*25)
