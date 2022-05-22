900505f9c92acd423ed67b41c8c762d80b7e5c84
suryadbs <105383429+suryadbs@users.noreply.github.com>
2022-05-22 14:26:25 +0530

    Add files via upload

---
+++ TS-2 PROGRAM 14.py
@@ -0,0 +1,13 @@
+
+def next(x):
+    print('The next Three Numbers of the number',x,'is :')
+    for i in range(1,4):
+        x=x+1
+        print('\t',x,end=('\t'))
+next(4)
+print('\n')
+next(6)
+print('\n')
+next(2+1)
+print('\n')
+next(4-3*2)
