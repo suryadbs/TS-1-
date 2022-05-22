a524e545b5cb5f8448d2e7b62d0a8ea8fa3e83f7
suryadbs <105383429+suryadbs@users.noreply.github.com>
2022-05-22 12:37:10 +0530

    Add files via upload

---
+++ TS-2 PROGRAM 10.py
@@ -0,0 +1,9 @@
+print('='*35)
+def volume(length,breadth,height):
+    print(' Volume of Box :',length*breadth*height,'Cubic Units')
+l=int(input(' Enter the Length of the Box  :'))
+b=int(input(' Enter the Breadth of the Box :'))
+h=int(input(' Enter the Height of the Box  :'))
+print()
+volume(l,b,h)
+print('='*35)
